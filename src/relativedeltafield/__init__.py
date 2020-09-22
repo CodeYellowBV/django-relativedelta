2bcbec2fd0bd0b7b206da7f7613c5f8e5704e73e
Gio <gustav8@dispostable.com>
2020-09-22 16:57:10 +0100

    Fixed missing coverage

--- src/relativedeltafield/__init__.py
+++ src/relativedeltafield/__init__.py
@@ -138,14 +138,9 @@ class RelativeDeltaField(models.Field):
 		fmt = 'to_char(%s, \'PYYYY"Y"MM"M"DD"DT"HH24"H"MI"M"SS.US"S"\')' % sql
 		return fmt, params
 
-	if django.VERSION < (2,):
-		def from_db_value(self, value, expression, connection, context=None):
-			if value is not None:
-				return parse_relativedelta(value)
-	else:
-		def from_db_value(self, value, expression, connection):
-			if value is not None:
-				return parse_relativedelta(value)
+	def from_db_value(self, value, expression, connection, context=None):
+		if value is not None:
+			return parse_relativedelta(value)
 
 	def value_to_string(self, obj):
 		val = self.value_from_object(obj)

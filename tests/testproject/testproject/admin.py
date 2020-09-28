cde1bfbf9552b375369be88b666b9807e3435300
Lie Ryan <ryan@prezzee.com.au>
2020-09-29 08:06:26 +1000

    Add admin for testproject.IntervalWithChoice

--- tests/testproject/testproject/admin.py
+++ tests/testproject/testproject/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from testapp.models import Interval
+from testapp.models import Interval, IntervalWithChoice
+
 
 @admin.register(Interval)
 class IntervalAdmin(admin.ModelAdmin):
+	list_display = ['value']
+	list_filter = ['value']
+
+
+@admin.register(IntervalWithChoice)
+class IntervalWithChoiceAdmin(admin.ModelAdmin):
 	list_display = ['value']
 	list_filter = ['value']

5f98c146f1f00dcc07e7d961bd29eb7c691eaa93
Peter Bex <peter@codeyellow.nl>
2020-10-07 16:54:27 +0200

    Some linting improvements

--- src/relativedeltafield/__init__.py
+++ src/relativedeltafield/__init__.py
@@ -1,11 +1,9 @@
 __version__ = '1.1.3'
 
-from .fields import RelativeDeltaField  # noqa
-
 from django.contrib.admin.options import FORMFIELD_FOR_DBFIELD_DEFAULTS
 
-from .fields import RelativeDeltaField
-from .forms import RelativeDeltaFormField
+from .fields import RelativeDeltaField # noqa
+from .forms import RelativeDeltaFormField # noqa
 
 FORMFIELD_FOR_DBFIELD_DEFAULTS[RelativeDeltaField] = {
     'form_class': RelativeDeltaFormField
--- src/relativedeltafield/fields.py
+++ src/relativedeltafield/fields.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
